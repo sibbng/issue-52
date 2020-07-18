43b97926db14874906bf9530e85e594dd78ac14d
Sibbngheid <sibbngheid@gmail.com>
2020-07-18 23:41:31 +0300

    add style to App.vue

--- src/App.vue
+++ src/App.vue
@@ -223,7 +223,7 @@
       </div>
     </nav>
 
-    <header class="bg-white shadow" v-if="$route.meta.title">
+    <header class="testing shadow" v-if="$route.meta.title">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">
           {{ $route.meta.title }}
@@ -249,3 +249,9 @@ export default defineComponent({
   }),
 })
 </script>
+
+<style>
+.testing {
+ background: red;
+}
+</style>
